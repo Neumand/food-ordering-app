9cf8e5d922a98edd5a321735d3d43964761133f3
David <david.m.neuman@gmail.com>
2019-03-27 17:37:02 +0000

    Created users migration.

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,7 +1,14 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments();
-    table.string('name');
+    table.string('first_name');
+    table.string('last_name');
+    table.integer('street_number');
+    table.string('street');
+    table.string('city');
+    table.string('province');
+    table.string('postal_code');
+    table.string('phone_number');
   });
 };
 
