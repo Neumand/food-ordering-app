9b9e8de3a109430a5acda0a076c9eb7c499dc83a
Marlon <m.de.la.cruz@hotmail.com>
2019-03-27 18:13:45 +0000

    added order migration

---
+++ db/migrations/20190327173906_create_order_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('orders', function (table) {
+    table.increments();
+    table.integer('users_id').unsigned()
+    table.integer('total');
+    table.integer('ready_time');
+    table.float('taxes');
+    table.foreign('users_id').references("users.id")
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('orders');
+};
